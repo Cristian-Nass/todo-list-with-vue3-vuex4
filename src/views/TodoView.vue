41074f80676803b6ef3de94aee40c14de8ec4b3b
Cristian Nass <cristian.tirdad@gmail.com>
2024-02-20 20:47:02 +0100

    add function

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -5,8 +5,14 @@
     <li v-for="item in getTodo.todo">
       {{ item.title }}
     </li> -->
-    <div>{{ message }}</div>
-    <div>{{ todo.loading }}</div>
+    <input class="input-form" type="text" name="title" ref="titleInput" />
+    <input
+      class="input-form"
+      type="text"
+      name="description"
+      ref="descriptionInput"
+    />
+    <button @click="addTodo" class="input-form">Submit</button>
     <li v-for="item in todo.todo">
       {{ item.title }}
     </li>
@@ -26,15 +32,38 @@ export default defineComponent({
   //   ...mapGetters(["getTodo"]),
   // },
   setup() {
-    const message = ref("Hello Vue!");
+    const titleInput = ref<HTMLInputElement | null>(null);
+    const descriptionInput = ref<HTMLInputElement | null>(null);
     const todo = computed<TodoList>(() => {
       return store.getters.getTodo;
     });
 
+    const addTodo = () => {
+      if (titleInput.value && descriptionInput.value) {
+        const title = titleInput.value.value;
+        const description = descriptionInput.value.value;
+        const todo = {
+          title,
+          description,
+          createdAt: new Date(),
+          done: false,
+        };
+        console.log(todo);
+      }
+    };
+
     return {
-      message,
+      titleInput,
+      descriptionInput,
       todo,
+      addTodo,
     };
   },
 });
 </script>
+<style scoped>
+.input-form {
+  padding: 4px;
+  margin: 4px;
+}
+</style>
